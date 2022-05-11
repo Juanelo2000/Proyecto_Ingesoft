3b5ddb29f4c5b1e84aacc055dabaa94b7f4ce447
Juanelo2000 <80127791+Juanelo2000@users.noreply.github.com>
2022-05-10 19:18:10 -0500

    pregunta

--- Fron_end/src/app/app.module.ts
+++ Fron_end/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PreguntaComponent } from './pregunta/pregunta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PreguntaComponent
   ],
   imports: [
     BrowserModule
