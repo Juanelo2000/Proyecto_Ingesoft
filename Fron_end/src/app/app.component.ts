2468d158908d6c60498f8fa14e3dbe6b0cdf0392
Miguelito-Poli <migarcia6@poligran.edu.co>
2022-05-17 19:51:28 -0500

    Primer acercamiento al formato
    Se hizo en el html y en el ts de APP, la primera prueba para ver como se veria refleajado el formato

--- Fron_end/src/app/app.component.ts
+++ Fron_end/src/app/app.component.ts
@@ -5,6 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent { //LO QUE HIZO MIGUEL
   title = 'Fron_end';
+  outTexto='';
+  outTitulo='';
+  getTexto(texto:string){
+    //console.warn(texto)
+    this.outTexto = texto
+  }
+  getTitulo(titulo:string){
+    //console.warn(val)
+    this.outTitulo = titulo
+  }
 }
